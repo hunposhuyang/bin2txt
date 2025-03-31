504b5fbcb977d6805a6d0c979e6f507c4791a5d6
魂魄淑漾 <2712411565@qq.com>
2025-04-01 04:27:27 +0800

    add build

---
+++ build/CMakeFiles/bin2txt.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bin2txt.dir/main.cpp.obj"
+  "CMakeFiles/bin2txt.dir/main.cpp.obj.d"
+  "bin2txt.exe"
+  "bin2txt.exe.manifest"
+  "bin2txt.pdb"
+  "libbin2txt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin2txt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
